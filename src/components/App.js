1479905680e13d663c72a12cac63b1a28ddf0484
bravo <eddi.bravo@ya.ru>
2016-09-03 19:50:20 +0300

    react компоненты

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './Post'
+import PostForm from './PostForm'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Post/>
+                <PostForm/>
+            </div>
+        )
+    }
+}
+export default App;
