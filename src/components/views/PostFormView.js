0c66b29a9b7477f0ddd934beb2ef5b2996000015
bravo <eddi.bravo@ya.ru>
2016-09-13 19:20:14 +0300

    пофиксил форму. Вынес реакцию на submit в отдельный метод, подправил редьюсер posts.

--- src/components/views/PostFormView.js
+++ src/components/views/PostFormView.js
@@ -6,37 +6,34 @@ class PostFormView extends React.Component {
         onPostSubmit: React.PropTypes.func.isRequired
     }
 
+    handleSubmit(event){
+        const { onPostSubmit  } = this.props
+        event.preventDefault();
+
+        if(!this._title.value.trim() || !this._body.value.trim() || !this._username.value.trim())
+            return;
+        onPostSubmit( { title: this._title.value.trim(), body: this._body.value.trim(), username: this._username.value.trim()} )
+        this._title.value = '';  this._body.value = ''; this._username.value = '' // и всё же как "правильно" чистить форму - так и не понял (
+    }
+
     render(){
-        let new_username, new_body, new_title
-        const { adding_new_post, onPostSubmit } = this.props
+        const { adding_new_post } = this.props
         return (
             <div>
                 <fieldset>
                     <legend>Добавление новости</legend>
 
-                    <form onSubmit={event => {
-
-                        event.preventDefault();
-
-                        if(!new_title.value.trim() || !new_body.value.trim() || !new_username.value.trim())
-                            return;
-                        onPostSubmit( { title: new_title.value.trim(), body: new_body.value.trim(), username: new_username.value.trim()} )
-
-                        new_title.value = ''
-                        new_body.value = ''
-                        new_username.value = ''
-
-                    }}>
+                    <form onSubmit={::this.handleSubmit}>
                         <div className="form-group">
-                            <input type="text" placeholder="your name" ref={node => {new_username = node}}  />
+                            <input type="text" placeholder="your name" ref={(node) => {this._username = node}}  />
                         </div>
 
                         <div className="form-group">
-                            <input type="text" placeholder="title" ref={node => {new_title = node}} />
+                            <input type="text" placeholder="title" ref={(node) => {this._title = node}} />
                         </div>
 
                         <div className="form-group">
-                            <textarea placeholder="message body" ref={node => {new_body = node}} />
+                            <textarea placeholder="message body" ref={(node) => {this._body = node}} />
                         </div>
 
                         <div className="form-group">
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -15,19 +15,27 @@ const initialState = {
 export default function posts(state=initialState, action) {
     switch(action.type)
     {
-        case REMOVE_POST_REQUEST:
+        case REMOVE_POST_REQUEST:{
             return state // todo
-        case REMOVE_POST_SUCCESS:
+        }
+        case REMOVE_POST_SUCCESS:{
             return{ ...state, items: state.items.filter(item => item.id !== action.payload.id) }
-        case FETCH_POSTS_REQUEST:
+        }
+        case FETCH_POSTS_REQUEST:{
             return{...state, fetching_posts: true}
-        case FETCH_POSTS_SUCCESS:
+        }
+        case FETCH_POSTS_SUCCESS:{
             return{...state, fetching_posts: false, items: action.payload}
-        case ADD_POST_REQUEST:
+        }
+        case ADD_POST_REQUEST:{
             return {...state, adding_new_post: true }
-        case ADD_POST_SUCCESS:
-            return {...state, adding_new_post: false, items: state.items.concat({ id: action.payload.id, title: action.payload.title, body: action.payload.body, username: action.payload.username}) }
-        default:
+        }
+        case ADD_POST_SUCCESS:{
+            const { id, title, username, body } = action.payload
+            return {...state, adding_new_post: false, items: state.items.concat({ id, title, body, username }) }
+        }
+        default:{
             return state
+        }
     }
 }
